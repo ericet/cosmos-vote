b0f4bc4e79f3677f63e017f09b9ffad4b06e416e
ericet <ericetchen@gmail.com>
2022-09-03 20:57:42 -0400

    filter out already voted proposals

--- src/components/KeplrVote.vue
+++ src/components/KeplrVote.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex flex-col justify-center items-center">
-    <button
-      type="button"
-      @click="handleVote"
-      class="mt-4 mb-8 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-    >
+    <button type="button" @click="handleVote"
+      class="mt-4 mb-8 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
       Vote
     </button>
   </div>
@@ -83,6 +80,7 @@ export default {
         }
       }
       if (ops.length > 0) {
+        try{
         let result = await client.signAndBroadcast(
           accounts[0].address,
           ops,
@@ -90,6 +88,9 @@ export default {
           ''
         );
         console.log(result);
+        }catch(err){
+          console.log(err)
+        }
       }else{
         console.log('all voted')
       }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,90 +3,50 @@
     <div class="mx-auto max-w-2xl">
       <header>
         <h1
-          class="text-6xl leading-normal mb-2 text-black font-serif text-gray-700 dark:text-gray-100 transition-colors"
-        >
+          class="text-6xl leading-normal mb-2 text-black font-serif text-gray-700 dark:text-gray-100 transition-colors">
           Vote
         </h1>
       </header>
-      <p
-        class="ml-4 mb-5 font-serif text-gray-700 dark:text-gray-100 transition-colors"
-      >
+      <p class="ml-4 mb-5 font-serif text-gray-700 dark:text-gray-100 transition-colors">
         Vote multiple proposals in one shot
       </p>
-      <h1
-        class="text-2xl leading-normal mb-2 text-black font-serif text-gray-700 dark:text-gray-100 transition-colors"
-      >
+      <h1 class="text-2xl leading-normal mb-2 text-black font-serif text-gray-700 dark:text-gray-100 transition-colors">
         Chain
       </h1>
       <div class="mb-3 relative text-gray-700">
-        <select
-          v-model="selected"
-          @change="getProposals"
-          class="w-full h-10 pl-3 pr-8 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline disabled:cursor-not-allowed"
-        >
+        <select v-model="selected" @change="getProposals"
+          class="w-full h-10 pl-3 pr-8 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline disabled:cursor-not-allowed">
           <option value="none" hidden>Select a Chain</option>
-          <!-- <option value="all" v-if="!useKeplr">All chains</option> -->
-          <option
-            v-for="option in options"
-            :key="option.value"
-            :value="option.value"
-          >
+          <option v-for="option in options" :key="option.value" :value="option.value">
             {{ option.text }}
           </option>
         </select>
-        <label
-          for="default-toggle"
-          class="inline-flex relative items-center mb-4 cursor-pointer mt-2 ml-2"
-        >
-          <input
-            type="checkbox"
-            v-model="useKeplr"
-            true-value="true"
-            false-value="false"
-            id="default-toggle"
-            class="sr-only peer"
-            @change="toggle"
-          />
+        <label for="default-toggle" class="inline-flex relative items-center mb-4 cursor-pointer mt-2 ml-2">
+          <input type="checkbox" v-model="useKeplr" true-value="true" false-value="false" id="default-toggle"
+            class="sr-only peer" @change="toggle" />
           <div
-            class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
-          ></div>
-          <span
-            class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Use Keplr</span
-          >
+            class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+          </div>
+          <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Use Keplr</span>
         </label>
       </div>
     </div>
-    <ProposalsList
-      :proposals="proposals"
-      class="mx-auto max-w-2xl"
-      v-if="proposals.length > 0"
-    />
-    <div
-      v-if="proposals.length == 0 && ready"
+    <ProposalsList :proposals="proposals" class="mx-auto max-w-2xl" v-if="proposals.length > 0" />
+    <div v-if="proposals.length == 0 && ready"
       class="mx-auto max-w-2xl mb-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-      role="alert"
-    >
-      <span class="block sm:inline">No proposals found!</span>
+      role="alert">
+      <span class="block sm:inline">No proposals found or all proposals have been voted!</span>
       <span class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="close">
-        <svg
-          class="fill-current h-6 w-6 text-red-500"
-          role="button"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
+        <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20">
           <title>Close</title>
           <path
-            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-          />
+            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
         </svg>
       </span>
     </div>
-    <MnemonicsInput
-      :proposals="proposals"
-      v-if="proposals.length > 0 && useKeplr === 'false'"
-    />
-    <KeplrVote :proposals="proposals" :selected="selected" v-if="proposals.length > 0 && useKeplr"/>
+    <MnemonicsInput :proposals="proposals" v-if="proposals.length > 0 && useKeplr === 'false'" />
+    <KeplrVote :proposals="proposals" :selected="selected" v-if="proposals.length > 0 && useKeplr" />
     <AppFooter />
   </div>
 </template>
@@ -97,6 +57,13 @@ import { chainsList } from '../config/chains';
 import MnemonicsInput from '../components/MnemonicsInput.vue';
 import AppFooter from '@/components/AppFooter.vue';
 import KeplrVote from '../components/KeplrVote.vue';
+import {
+  QueryClient,
+  setupGovExtension,
+  setupBankExtension,
+} from '@cosmjs/stargate';
+import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
+
 export default {
   created() {
     for (let chain of chainsList) {
@@ -132,7 +99,30 @@ export default {
         this.options.push({ text: chain.name, value: chain.value });
       }
     },
-    getProposals() {
+    hasVoted(client, proposal, address) {
+      return new Promise((resolve) => {
+        client.gov
+          .vote(proposal.proposal_id, address)
+          // eslint-disable-next-line no-unused-vars
+          .then((res) => {
+            resolve(true);
+          })
+          // eslint-disable-next-line no-unused-vars
+          .catch((err) => {
+            resolve(false);
+          });
+      });
+    },
+    async getQueryClient(rpcEndpoint) {
+      const tendermint34Client = await Tendermint34Client.connect(rpcEndpoint);
+      const queryClient = QueryClient.withExtensions(
+        tendermint34Client,
+        setupBankExtension,
+        setupGovExtension
+      );
+      return queryClient;
+    },
+    async getProposals() {
       this.ready = false;
       this.proposals = [];
       if (this.selected === 'none') {
@@ -162,22 +152,37 @@ export default {
         }
         this.ready = true;
       } else {
+        let chain = this.$store.state.chainMap.get(this.selected);
+        await window.keplr.enable(chain.id);
+        const offlineSigner = window.getOfflineSigner(chain.id);
+        const accounts = await offlineSigner.getAccounts();
+        console.log(accounts[0].address);
+        const queryClient = await this.getQueryClient(chain.rpc);
         axios
           .get(
             `https://rest.cosmos.directory/${this.selected}/cosmos/gov/v1beta1/proposals?pagination.limit=3000`
           )
-          .then((res) => {
+          .then(async (res) => {
             let proposals = res.data.proposals.filter((proposal) => {
               return proposal.status === 'PROPOSAL_STATUS_VOTING_PERIOD';
             });
             if (proposals.length > 0) {
               let chain = this.$store.state.chainMap.get(this.selected);
               for (let proposal of proposals) {
-                proposal.chain = chain.value;
-                proposal.chain_name = chain.name;
-                proposal.vote = '1';
+                let voted = await this.hasVoted(
+                  queryClient,
+                  proposal,
+                  accounts[0].address
+                );
+                console.log(voted)
+                if(!voted){
+                  proposal.chain = chain.value;
+                  proposal.chain_name = chain.name;
+                  proposal.vote = '1';
+                  this.proposals.push(proposal)
+                }
               }
-              this.proposals = this.proposals.concat(proposals);
+              // this.proposals = this.proposals.concat(proposals);
             }
             this.ready = true;
           })
